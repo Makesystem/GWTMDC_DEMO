e14deb9f6e80f20bc290ca6fceb90ee4ce6b1ca1
Richeli Vargas <Richeli Vargas@DEV-02.makesystem.srv>
2019-10-07 11:21:26 -0300

    Melhorias

--- src/main/java/gwt/material/design/components/demo/client/GWTMDCDemo.java
+++ src/main/java/gwt/material/design/components/demo/client/GWTMDCDemo.java
@@ -1,13 +1,11 @@
 package gwt.material.design.components.demo.client;
 
-import java.util.Date;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.LocaleInfo;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.makesystem.pidgey.console.Console;
+import com.makesystem.pidgey.console.ConsoleColor;
+import com.makesystem.pidgey.io.net.IpAddressGWT;
 
 import gwt.material.design.components.client.theme.MaterialThemes;
 import gwt.material.design.components.client.theme.ThemeManager;
@@ -17,18 +15,25 @@ import gwt.material.design.components.demo.client.app.DemoApp;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GWTMDCDemo implements EntryPoint {
-
+	
 	public void onModuleLoad() {
-
+		
 		// GWT.log("IP: " + publicIp());
-
-		// RootPanel.get().add(new DemoApp());
+		try {
+			final IpAddressGWT ip = new IpAddressGWT();
+			final String publicIp = ip.getPublic();
+			Console.log("IP: {cc}{s}", ConsoleColor.BLUE, publicIp);
+		} catch (Throwable e) {
+			Console.log("IP: {cc}{s}", ConsoleColor.RED, e.getMessage());
+		}
+		
+		Console.log("Current Locale: {cc}{s}", ConsoleColor.BLUE, LocaleInfo.getCurrentLocale().getLocaleName());
+		
+		RootPanel.get().add(new DemoApp());
+		ThemeManager.applyTheme(MaterialThemes.INSTANCE.dark_amber());
 		// ThemeManager.applyTheme(IResources.INSTANCE.theme());
-		// ThemeManager.applyTheme(MaterialThemes.INSTANCE.dark_amber());
-
-		Console.log("Current Locale: {dt}", new Date());
-		Console.log("Current Locale: {s}", LocaleInfo.getCurrentLocale().getLocaleName());
-
+		
+		
+		
 	}
-
 }
